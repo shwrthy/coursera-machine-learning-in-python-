c6754a26782228d6ed7abc2351afd581e87b1748
uniform12123 <revolver.johnny.e@gmail.com>
2019-07-14 14:45:52 +0800

    Delete computeCost.py

--- computeCost.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-# Instructions: Compute the cost of a particular choice of theta
-#               You should set J to the cost.
-def computeCost(X, y, theta):
-    m = np.size (X, 0)
-    delta = np.dot(X, theta) - y
-    delta = np.power(delta, 2)
-    return 0.5 / m * delta.sum()
