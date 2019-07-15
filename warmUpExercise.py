53b82e49a79d48217b8b8ffccf67062540c7f75c
uniform12123 <revolver.johnny.e@gmail.com>
2019-07-15 14:57:53 +0800

    Delete warmUpExercise.py

--- warmUpExercise.py
+++
@@ -1,9 +0,0 @@
-# Instructions: Return the 5x5 identity matrix 
-#               In octave, we return values by defining which variables
-#               represent the return values (at the top of the file)
-#               and then set them accordingly
-import numpy as np
-def warmUpExercise ():
-    A = np.eye (5)
-    print (A)
-    return
